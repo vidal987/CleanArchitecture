a26882058f65985ac18b7b5ab183847a75cf06ce
vidal987 <mvsouza.af@gmail.com>
2021-05-26 15:54:26 -0300

    Add: Testes

---
+++ CleanArch.Domain.Tests/CategoryUnitTest1.cs
@@ -0,0 +1,54 @@
+using CleanArch.Domain.Entities;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace CleanArch.Domain.Tests
+{
+    public class CategoryUnitTest1
+    {
+        [Fact(DisplayName = "Create category with valid state")]
+        public void CreateCategory_WithValidParameters_ResultObjectValidState()
+        {
+            Action action = () => new Category(1, "Category1");
+            action.Should()
+                .NotThrow<CleanArch.Domain.Validation.DomainExceptionValidation>();
+        }
+
+        [Fact]
+        public void CreateCategory_NegativeIdValeu_DomainExceptionInvalidId()
+        {
+            Action action = () => new Category(-1, "Category1");
+            action.Should()
+                .Throw<CleanArch.Domain.Validation.DomainExceptionValidation>().WithMessage("Invalid Id value");
+        }
+
+        [Fact]
+        public void CreateCategory_ShortNameValue_DomainExceptionShortName()
+        {
+            Action action = () => new Category(1, "ca");
+            action.Should()
+                .Throw<CleanArch.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name. too short, minimum 3 characters");
+        }
+
+        [Fact]
+        public void CreateCategory_MissingNameValue_DomainExceptionRequiredName()
+        {
+            Action action = () => new Category(1, "");
+            action.Should()
+                .Throw<CleanArch.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name. Name is required");
+        }
+
+        [Fact]
+        public void CreateCategory_WithNullNameValue_DomainExceptionInvalidName()
+        {
+            Action action = () => new Category(1, null);
+            action.Should()
+                .Throw<CleanArch.Domain.Validation.DomainExceptionValidation>();
+        }
+
+
+    }
+}
--- CleanArch.Domain/Entities/Product.cs
+++ CleanArch.Domain/Entities/Product.cs
@@ -1,36 +1,26 @@
 ﻿using CleanArch.Domain.Validation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CleanArch.Domain.Entities
 {
-    //SEALED GARANTE QUE A CLASSE NÃO PODE SER HERDADA
-    //PRIVATE garante que as propriedades não podem ser alterardas externamente
     public sealed class Product : Entity
     {
         public string Name { get; private set; }
-        public string  Description { get; private set; }
+        public string Description { get; private set; }
         public decimal Price { get; private set; }
         public int Stock { get; private set; }
         public string Image { get; private set; }
 
-        #region Contrutores parametrizados
         public Product(string name, string description, decimal price, int stock, string image)
         {
             ValidateDomain(name, description, price, stock, image);
         }
 
-        public Product(int id, string name, string description, decimal price, int stock, string image, int categoryId, Category category)
+        public Product(int id, string name, string description, decimal price, int stock, string image)
         {
-
-            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            DomainExceptionValidation.When(id < 0, "Invalid Id value.");
             Id = id;
             ValidateDomain(name, description, price, stock, image);
         }
-        #endregion
 
         public void Update(string name, string description, decimal price, int stock, string image, int categoryId)
         {
@@ -38,37 +28,36 @@ namespace CleanArch.Domain.Entities
             CategoryId = categoryId;
         }
 
-
-        #region Validação de dominio
-        public void ValidateDomain(string name, string description, decimal price, int stock, string image)
+        private void ValidateDomain(string name, string description, decimal price, int stock, string image)
         {
             DomainExceptionValidation.When(string.IsNullOrEmpty(name),
                 "Invalid name. Name is required");
 
             DomainExceptionValidation.When(name.Length < 3,
-                "Invalid name. too short, minimum 3 characters");
+                "Invalid name, too short, minimum 3 characters");
 
             DomainExceptionValidation.When(string.IsNullOrEmpty(description),
-               "Invalid description. description is required");
+                "Invalid description. Description is required");
 
             DomainExceptionValidation.When(description.Length < 5,
-                "Invalid description. too short, minimum 5 characters");
+                "Invalid description, too short, minimum 5 characters");
 
             DomainExceptionValidation.When(price < 0, "Invalid price value");
 
-            DomainExceptionValidation.When(stock < 0, "Invalid price value");
+            DomainExceptionValidation.When(stock < 0, "Invalid stock value");
 
-            DomainExceptionValidation.When(image.Length > 250,
-                "Invalid image name. too long. maximum 250 characters");
+            DomainExceptionValidation.When(image?.Length > 250,
+                "Invalid image name, too long, maximum 250 characters");
 
             Name = name;
             Description = description;
             Price = price;
             Stock = stock;
             Image = image;
+
         }
-        #endregion
+
         public int CategoryId { get; set; }
-        public Category category { get; set; }
+        public Category Category { get; set; }
     }
 }
